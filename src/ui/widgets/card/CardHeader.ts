ddd083a6513520f4c80c909d794ed5f935514a95
peter fisk <peter.fisk@gmail.com>
2024-12-23 09:33:16 -0500

    card header

--- src/ui/widgets/card/CardHeader.ts
+++ src/ui/widgets/card/CardHeader.ts
@@ -5,7 +5,7 @@ import { Column } from "../column";
 import { Row } from "../row";
 
 export class CardHeader extends FluidContainer {
-    titleRow:Row;
+    titleRow: Row;
 
     constructor(title: string, buttonLabel: string | null = null) {
         super();
@@ -16,24 +16,38 @@ export class CardHeader extends FluidContainer {
     addClasses() {
         super.addClasses();
         this.addClass('card-header');
+        this.addClass('d-flex');
+        this.addClass('flex-row');
+        this.addClass('bd-highlight');
     }
 
     addTitleRow(title: string) {
-        const column = new Column(10);
+        // const column = new Column(10);
+        const leftContainer = new FluidContainer;
+        leftContainer.addClass('flex-fill');
+        leftContainer.addClass('flex-row');
+        leftContainer.setBackgroundColor('orange');
         const h3 = new H3Element(title);
-        h3.addClass('w-75');
-        h3.addClass('p-3');
-        column.addChild(h3);
-        this.titleRow.addChild(column);
-        this.addChild(this.titleRow);
+        leftContainer.addChild(h3);
+        this.addChild(leftContainer);
+        // h3.addClass('w-75');
+        // h3.addClass('p-3');
+        // column.addChild(h3);
+        // this.titleRow.addChild(column);
+        // this.addChild(this.titleRow);
     }
 
     addTopMenuButton() {
-        const column = new Column(2);
-        column.addClass('float-right');
+        // const column = new Column(2);
+        const rightContainer = new FluidContainer;
+        rightContainer.setBackgroundColor('honeydew');
+        rightContainer.addClass('flex-fill')
+        rightContainer.addClass('flex-row');
         const button = new Button('Top Menu');
-        column.addChild(button);
-        this.titleRow.addChild(column);
+        button.addClass('float-end');
+        rightContainer.addChild(button);
+        this.addChild(rightContainer);
+        // this.titleRow.addChild(column);
     }
 
 }
