d18eea300b8c954de4da41e01fd34c149f0a22fd
peter fisk <peter.fisk@gmail.com>
2024-12-24 14:14:06 -0500

    textpanel

--- src/ui/widgets/panels/TextPanel.ts
+++ src/ui/widgets/panels/TextPanel.ts
@@ -1,7 +1,27 @@
-import { FillContainer } from "../../containers/FlillContainer";
+import { TextAreaElement } from "../../core/elements/TextAreaElement";
 import { Panel } from "./Panel";
 
 export class TextPanel extends Panel {
+    textArea?: TextAreaElement;
 
+    constructor() {
+        super();
+    }
+
+    addChildren() {
+        super.addChildren();
+        if (this.textArea)
+            this.addChild(this.textArea);
+    }
+
+    initialize() {
+        super.initialize();
+        this.textArea = new TextAreaElement;
+        this.textArea.setPosition('absolute');
+        this.textArea.setTop(0);
+        this.textArea.setLeft(0);
+        this.textArea.setRight(0);
+        this.textArea.setBottom(0);
+    }
 
 }
