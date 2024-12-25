7090b2019d21e915763cbad0d2e91e676728cf75
peter fisk <peter.fisk@gmail.com>
2024-12-25 16:51:01 -0500

    projects store

--- src/ui/pages/projects/ProjectsPage.ts
+++ src/ui/pages/projects/ProjectsPage.ts
@@ -1,12 +1,21 @@
 import { ProjectsStore } from "../../../data/stores/ProjectsStore";
+import { AbstractElement } from "../../core";
+import { List } from "../../widgets/list/List";
 import { Page } from "../Page";
 
 export class ProjectsPage extends Page {
+    list:List|null = null;
     projectsStore: ProjectsStore;
 
     constructor() {
         super();
         this.projectsStore = ProjectsStore.getInstance();
+    }
+
+    defaultContent(): AbstractElement {
+        if (!this.list)
+            this.list = new List;
+        return this.list;
     }
 
     defaultTitle(): string {
