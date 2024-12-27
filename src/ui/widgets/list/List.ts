0218b3e685d2a1e2e91b51aee0cd36f0719b59a5
peter fisk <peter.fisk@gmail.com>
2024-12-27 13:28:34 -0500

    page scrolling

--- src/ui/widgets/list/List.ts
+++ src/ui/widgets/list/List.ts
@@ -2,4 +2,9 @@ import { UlElement } from "../../core/elements/UlElement";
 
 export class List extends UlElement {
 
+    addClasses() {
+        super.addClasses();
+        this.addClass('overflow-visible');
+    }
+    
 }
