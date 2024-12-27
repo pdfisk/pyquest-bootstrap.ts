2b30c8d7cc442e41e8de922acfae6ece0e53fdde
peter fisk <peter.fisk@gmail.com>
2024-12-27 12:37:35 -0500

    heroku url

--- src/constants/UrlConstants.ts
+++ src/constants/UrlConstants.ts
@@ -1,5 +1,4 @@
 export class UrlConstants {
     static localProjects: string = 'http://localhost:9080/api/projects';
-    static remoteProjects: string = 'https://vistapython-7ee93adc9411.herokuapp.com';
- 
+    static remoteProjects: string = 'https://vistapython-7ee93adc9411.herokuapp.com/api/projects';
 }
